c2dc72fffa144a36731b660a758b89f669ac453a
ahtrahdis7 <sidharthabiki@gmail.com>
2021-05-31 10:46:54 +0530

    add: lru-cache-code

---
+++ index.js
@@ -0,0 +1,103 @@
+/**
+ * Doubly Linked List Implementation
+ */
+class Node {
+    constructor(key, value, next = null, prev = null) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.size = 0;
+    this.limit = capacity;
+    this.cache = {};
+    this.head = null;
+    this.tail = null;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    
+    if(this.cache[key]){
+        const value = this.cache[key].value;
+
+        // node removed from it's position and cache
+        this.remove(key)
+        // this.size--;
+        // write node again to the head of LinkedList to make it most recently used
+        this.put(key, value);
+
+        return value;
+    }
+    return -1;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+
+    if(!this.head){
+    this.head = this.tail = new Node(key, value);
+    }else{
+    const node = new Node(key, value, this.head);
+    this.head.prev = node;
+    this.head = node;
+    }
+
+    //Update the cache map
+    if(this.cache[key]){
+        this.remove(key);
+        this.cache[key] = this.head;
+        this.size++;
+    }else{
+        this.ensureLimit();
+        this.cache[key] = this.head;
+        this.size++;
+    }
+    
+};
+
+LRUCache.prototype.ensureLimit = function() {
+    if(this.size == this.limit){
+        this.remove(this.tail.key);
+    }
+};
+
+LRUCache.prototype.remove = function(key) {
+    const node = this.cache[key];
+    
+    if(node == undefined) return;
+    // console.log(node)
+    
+    if(node.prev !== null){
+    node.prev.next = node.next;
+    }else{
+    this.head = node.next;
+    }
+    
+    if(node.next !== null){
+    node.next.prev = node.prev;
+    }else{
+    this.tail = node.prev
+    }
+    delete this.cache[key];
+    this.size--;
+};
+
+// exports.Cache = LRUCache;
+
+exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+  }
